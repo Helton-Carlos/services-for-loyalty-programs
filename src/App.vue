cbe53597a9c3cc3d07dd2fc6040ca9e8a2e32083
Helton Brito <heltoncarlospa@hotmail.com>
2023-05-31 21:31:23 -0300

    chore: add project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
