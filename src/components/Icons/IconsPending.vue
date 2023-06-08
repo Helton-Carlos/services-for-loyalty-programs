d159458aab57797fcb938a066f073c5dfff0acf3
Helton Brito <heltoncarlospa@hotmail.com>
2023-06-08 14:28:00 -0300

    feat: add icons

---
+++ src/components/Icons/IconsPending.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg
+    class="cursor-pointer"
+    width="44"
+    height="45"
+    viewBox="0 0 44 45"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect y="0.5" width="44" height="44" rx="22" fill="#34C759" />
+    <path d="M33.4666 11H9.99994V34.4667H33.4666V11Z" fill="#34C759" />
+    <path
+      d="M21.7332 31.7779C16.7368 31.7779 12.6888 27.7299 12.6888 22.7334C12.6888 17.737 16.7368 13.689 21.7332 13.689C26.7297 13.689 30.7777 17.737 30.7777 22.7334C30.7777 27.7299 26.7297 31.7779 21.7332 31.7779Z"
+      fill="#34C759"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M21.2835 24.2783L19.5626 22.5574"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M24.7253 20.8367L21.2835 24.2785"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
---
+++ src/components/Icons/IconsRedeemed.vue
@@ -0,0 +1,82 @@
+<template>
+  <svg
+    class="cursor-pointer"
+    width="44"
+    height="45"
+    viewBox="0 0 44 45"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect y="0.5" width="44" height="44" rx="22" fill="#AF52DE" />
+    <path
+      d="M22.9279 21.9804V32H15.1115C13.9443 32 13 31.0558 13 29.8886V21.9804C13 20.8132 13.9443 19.8689 15.1115 19.8689H20.8164C21.977 19.8689 22.9279 20.8132 22.9279 21.9804Z"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M25.1311 30.6099V27.246C25.1311 26.1312 24.141 25.2263 22.9279 25.2263"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M20.5148 32H23.7738"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M22.9279 29.1411V30.9903C22.9279 31.5477 23.4197 32.0001 24.0295 32.0001C24.6393 32.0001 25.1311 31.5477 25.1311 30.9903V29.1411"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M15.6885 22.5706H20.4033"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M15.6885 25.9346H20.4033"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M15.7082 29.2917H17.0525"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M26.1148 19.8689C28.2877 19.8689 30.0492 18.1074 30.0492 15.9344C30.0492 13.7615 28.2877 12 26.1148 12C23.9418 12 22.1803 13.7615 22.1803 15.9344C22.1803 18.1074 23.9418 19.8689 26.1148 19.8689Z"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M26.1148 16.2832L24.8387 15.407"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M26.1148 16.2833L27.8629 14.969"
+      stroke="#161A2E"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
